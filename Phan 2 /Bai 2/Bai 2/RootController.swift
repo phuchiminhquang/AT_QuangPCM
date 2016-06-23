ea329aa8db20e416a1030d9c82bba209c516a024
Quang Phu <phuchiminhquang@gmail.com>
2016-06-23 15:06:37 +0700

    The first exam

---
+++ Phan 2 /Bai 2/Bai 2/RootController.swift
@@ -0,0 +1,35 @@
+//
+//  RootController.swift
+//  Bai 1
+//
+//  Created by Quang Phu on 6/23/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+import UIKit
+
+class RootController: UIViewController {
+
+    @IBOutlet weak var lbMessage: UILabel!
+    
+    @IBOutlet weak var tfFirstName: UITextField!
+    
+    @IBOutlet weak var tfLastName: UITextField!
+    
+    @IBOutlet weak var btnSubmit: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+    
+}
