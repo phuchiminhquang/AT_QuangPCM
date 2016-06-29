49776c7be6ebf95c6d33484774fe5ed3767e8f72
Quang Phu <phuchiminhquang@gmail.com>
2016-06-29 14:06:59 +0700

    Phan 4 - Bai 9

---
+++ Phan 4/Bai 9/Bai 9/RootController.swift
@@ -0,0 +1,36 @@
+
+//
+//  RootController.swift
+//  Bai 9
+//
+//  Created by Quang Phu on 6/29/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+import UIKit
+
+class RootController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Phan 4/Bai 9/Bai 9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bai 9
+//
+//  Created by Quang Phu on 6/29/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
