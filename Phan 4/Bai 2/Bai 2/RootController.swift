4116fdf10f8c342b3cdfe62426e2fa285f1116c8
Quang Phu <phuchiminhquang@gmail.com>
2016-06-29 10:42:40 +0700

    Phan 4: Bai 2

---
+++ Phan 4/Bai 2/Bai 2/RootController.swift
@@ -0,0 +1,26 @@
+//
+//  RootController.swift
+//  Bai 2
+//
+//  Created by Quang Phu on 6/29/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+import UIKit
+
+class RootController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+            }
+    
+
+
+
+}
---
+++ Phan 4/Bai 2/Bai 2/RootVC.swift
@@ -0,0 +1,25 @@
+//
+//  RootVC.swift
+//  Bai 2
+//
+//  Created by Quang Phu on 6/29/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
