3b3c7e650673c3dafd705a202c147d009f798a8d
Quang Phu <phuchiminhquang@gmail.com>
2016-06-29 14:32:17 +0700

    Phan 4 - Bai 10

---
+++ Phan 4/Bai 10/Bai 10/AppDelegate.swift
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.swift
+//  Bai 10
+//
+//  Created by Quang Phu on 6/29/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        if let window = window {
+            let root = RootController(nibName: "RootController", bundle: nil)
+//            let navigation = UINavigationController(rootViewController: root)
+            window.rootViewController = root
+            
+            window.backgroundColor = UIColor.whiteColor()
+            window.makeKeyAndVisible()
+        }
+        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+       
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        
+    }
+
+
+
+}
+
---
+++ Phan 4/Bai 10/Bai 10/RootController.swift
@@ -0,0 +1,40 @@
+//
+//  RootController.swift
+//  Bai 10
+//
+//  Created by Quang Phu on 6/29/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+import UIKit
+
+class RootController: UIViewController {
+
+    // MARK: Property
+    
+    
+    // MARK: Init View
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    // MARK: private function
+    
+
+   
+}
