88418af72a400b09754595f503bb093d6571704c
Quang Phu <phuchiminhquang@gmail.com>
2016-06-29 10:51:37 +0700

    Phan 4 - Bai 3

---
+++ Phan 4/Bai 3/Bai 3/RootController.swift
@@ -0,0 +1,37 @@
+//
+//  RootController.swift
+//  Bai 3
+//
+//  Created by Quang Phu on 6/29/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+import UIKit
+
+class RootController: UIViewController {
+
+    // MARK: Property
+    
+    
+    // MARK: Init View
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    // MARK: private function
+}
