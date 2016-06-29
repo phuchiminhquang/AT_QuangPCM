3bd581f0928db82553d4333e0b8964cb18c977e7
Quang Phu <phuchiminhquang@gmail.com>
2016-06-29 11:32:29 +0700

    Phan 4 - Bai 7

---
+++ Phan 4/Bai 7/Bai 7/RootController.swift
@@ -0,0 +1,24 @@
+//
+//  RootController.swift
+//  Bai 7
+//
+//  Created by Quang Phu on 6/29/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+import UIKit
+
+class RootController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
