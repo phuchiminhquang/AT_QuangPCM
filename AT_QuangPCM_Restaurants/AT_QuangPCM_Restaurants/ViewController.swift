cd492998ce1c4034be78182d951226581ef8af3f
Quang Phu <phuchiminhquang@gmail.com>
2016-07-14 10:31:06 +0700

    no message

---
+++ AT_QuangPCM_Restaurants/AT_QuangPCM_Restaurants/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AT_QuangPCM_Restaurants
+//
+//  Created by Quang Phu on 7/14/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
